6a7d32d39096a88378039933b4e2a182ec773fcc
richardo2016x <richardo2016x@gmail.com>
2022-09-16 18:46:40 +0800

    feat: update home page's style.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,11 @@
-import React, { useMemo } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.less';
 import DApps from './routes/Dapps';
-
-function useTabs(rabbyExt: Electron.Extension | null) {
-  return useMemo(() => {
-    return [].filter(Boolean);
-  }, []);
-}
 
 export default function App() {
   return (
     <Router initialEntries={['/dapps']}>
       <Routes>
-        {/* <Route path="/" element={<DApps />} /> */}
         <Route path="/dapps" element={<DApps />} />
       </Routes>
     </Router>
