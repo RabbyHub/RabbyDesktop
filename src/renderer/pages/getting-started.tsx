9cae7cb1f0ee560fad3fbfc50457bbefc7b7c02b
Hodor <lost1q84@gmail.com>
2022-09-20 11:01:28 +0800

    feat: add get start page

--- src/renderer/pages/getting-started.tsx
+++ src/renderer/pages/getting-started.tsx
@@ -5,23 +5,29 @@ import '../css/style.less';
 
 import './getting-started.less';
 import { useDesktopAppState } from '../hooks/useDesktopAppState';
+import React from 'react';
+import { Button } from 'antd';
 
 export default function GettingStarted() {
   const { redirectToMainWindow } = useDesktopAppState();
 
   return (
-    <div>
-      global
-
-      <button onClick={() => {
-        // putHasStarted();
-
-        redirectToMainWindow();
-      }}>Getting Started</button>
+    <div className="page-welcome">
+      <div className="page-content">
+        <div className="slogan">Specialized client for Dapp security</div>
+        <div className="slogan">Rabby Wallet Desktop</div>
+      </div>
+      <Button
+        onClick={() => {
+          // putHasStarted();
+          redirectToMainWindow();
+        }}
+      >
+        Getting Started
+      </Button>
     </div>
   );
 }
-
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
