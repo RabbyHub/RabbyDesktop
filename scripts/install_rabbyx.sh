464ad400b76183c09d24ed05a5f60364eabadc8d
richardo2016x <richardo2016x@gmail.com>
2022-12-19 22:51:34 +0800

    feat: use packed rabbyx rather than development

---
+++ scripts/install_rabbyx.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+scripts_dir="$( cd "$( dirname "$0"  )" && pwd  )"
+project_dir=$(dirname "$script_dir")
+assets_dir="$( dirname "$scripts_dir"  )/assets"
+
+echo "[install_rabbyx] try to download rabbyx.zip..."
+curl -sL https://download.rabby.io/_tools/RabbyX-v0.60.1-036fdb88.zip > $assets_dir/rabbyx.zip
+# curl -sL https://download.rabby.io/_tools/RabbyX-latest.zip > $assets_dir/rabbyx.zip
+
+mkdir -p $assets_dir/chrome_exts/
+echo "[install_rabbyx] clean old plugin..."
+rm -rf $assets_dir/chrome_exts/rabby;
+unzip $assets_dir/rabbyx.zip -d $assets_dir/chrome_exts/rabby/
+
+echo "[install_rabbyx] clean..."
+rm -f $assets_dir/rabbyx.zip;
+
+echo "[install_rabbyx] finished!"
