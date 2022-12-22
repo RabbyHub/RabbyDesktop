74d45b08e0187ea0b4184dbda5c36fbc6db17bfe
heisenberg <heisenberg-2077@outlook.com>
2022-12-22 10:25:37 +0800

    fix: get started

--- src/renderer/routes/Welcome/GettingStarted.tsx
+++ src/renderer/routes/Welcome/GettingStarted.tsx
@@ -1,8 +1,4 @@
-/// <reference path="../../preload.d.ts" />
-
-import '@/renderer/css/style.less';
-
-import { Button } from 'antd';
+import { Button, Col, Row } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import styles from './GettingStarted.module.less';
 
@@ -10,21 +6,29 @@ export default function GettingStarted() {
   const nav = useNavigate();
 
   return (
-    <div className={styles['page-welcome']}>
-      <div className={styles['page-content']}>
-        <div className={styles.slogan}>
-          Specialized client for Dapp security
+    <Row className={styles['page-welcome']} align="middle">
+      <Col className={styles.container} span={17} offset={3}>
+        <div className={styles['page-content']}>
+          <div className={styles.slogan}>
+            Specialized client for Dapp security
+          </div>
+          <div className={styles.slogan}>Rabby Wallet Desktop</div>
         </div>
-        <div className={styles.slogan}>Rabby Wallet Desktop</div>
-      </div>
-      <Button
-        className={styles['btn-start']}
-        onClick={() => {
-          nav('/welcome/import/home');
-        }}
-      >
-        Getting Started
-      </Button>
-    </div>
+        <Button
+          type="primary"
+          className={styles['btn-start']}
+          onClick={() => {
+            nav('/welcome/import/home');
+          }}
+        >
+          Get started
+        </Button>
+      </Col>
+      <img
+        src="rabby-internal://assets/icons/common/logo.svg"
+        alt="logo"
+        className={styles.logo}
+      />
+    </Row>
   );
 }
