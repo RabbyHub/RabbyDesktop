ff97b6879b3bfccf3776353a2c7435d285a8465e
richardo2016x <richardo2016x@gmail.com>
2023-01-03 15:36:44 +0800

    feat: adjust style of Settings Page.

---
+++ assets/icons/mainwin-settings/index.ts
@@ -0,0 +1,2 @@
+export { default as IconChevronRight } from './item-chevron-right.svg';
+export { default as IconLink } from './item-link.svg';
--- src/renderer/routes/Settings/index.tsx
+++ src/renderer/routes/Settings/index.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { openExternalUrl } from '@/renderer/ipcRequest/app';
 
 import { useAppVersion } from '@/renderer/hooks/useMainBridge';
+import { IconChevronRight, IconLink } from '@/../assets/icons/mainwin-settings';
+
 import styles from './index.module.less';
 
 type TypedProps = {
@@ -51,11 +53,7 @@ function ItemLink({
       <ItemPartialLeft name={props.name} icon={props.icon} />
       <div className={styles.itemRight}>
         <div className={styles.itemArrow}>
-          {useChevron ? (
-            <img src="rabby-internal://assets/icons/mainwin-settings/chevron-right.svg" />
-          ) : (
-            <img src="rabby-internal://assets/icons/mainwin-settings/link.svg" />
-          )}
+          {useChevron ? <img src={IconChevronRight} /> : <img src={IconLink} />}
         </div>
       </div>
     </div>
@@ -79,22 +77,39 @@ export function MainWindowSettings() {
 
   return (
     <div className={styles.settingsPage}>
-      {/* Update Area */}
+      {/* TODO: implement Update Area */}
       <div />
 
       <div className={styles.settingBlock}>
         <h4 className={styles.blockTitle}>About</h4>
         <div className={styles.itemList}>
-          <ItemText name="Version" text={appVerison || '-'} />
+          <ItemText
+            name="Version"
+            text={appVerison || '-'}
+            icon="rabby-internal://assets/icons/mainwin-settings/info.svg"
+          />
           {/* <ItemLink name='User Agreement' /> */}
           <ItemLink
-            name="Privay Policy"
+            name="Privacy Policy"
             link="https://rabby.io/docs/privacy/"
+            icon="rabby-internal://assets/icons/mainwin-settings/privacy.svg"
             useChevron
           />
-          <ItemLink name="Website" link="https://rabby.io/" />
-          <ItemLink name="Discord" link="https://discord.gg/seFBCWmUre" />
-          <ItemLink name="Twitter" link="https://twitter.com/Rabby_io" />
+          <ItemLink
+            name="Website"
+            link="https://rabby.io/"
+            icon="rabby-internal://assets/icons/mainwin-settings/homesite.svg"
+          />
+          <ItemLink
+            name="Discord"
+            link="https://discord.gg/seFBCWmUre"
+            icon="rabby-internal://assets/icons/mainwin-settings/discord.svg"
+          />
+          <ItemLink
+            name="Twitter"
+            link="https://twitter.com/Rabby_io"
+            icon="rabby-internal://assets/icons/mainwin-settings/twitter.svg"
+          />
         </div>
       </div>
     </div>
