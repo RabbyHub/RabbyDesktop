313c2fcc73696eb400cc498924b9fff779efb6b8
richardo2016x <richardo2016x@gmail.com>
2023-01-16 16:46:56 +0800

    chore: avoid frequent react-refresh overlay.

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -284,17 +284,6 @@ type ChannelMessagePayload = {
     ];
   };
 
-  [`__internal_rpc:rabbyx-rpc:query`]: {
-    send: [reqId: string, query: Omit<IRabbyxRpcQuery, 'rpcId'>];
-    response: [
-      {
-        reqId: string;
-        result: any;
-        error?: Error;
-      }
-    ];
-  };
-
   [`rabbyx-rpc-respond`]: {
     send: [string | IRabbyxRpcResponse];
     response: [];
@@ -407,6 +396,14 @@ type ChannelInvokePayload = {
   'apply-proxyConfig': {
     send: [conf: IAppProxyConf];
     response: void;
+  };
+
+  [`__internal_rpc:rabbyx-rpc:query`]: {
+    send: [query: Omit<IRabbyxRpcQuery, 'rpcId'>];
+    response: {
+      result: any;
+      error?: Error;
+    };
   };
 };
 type IInvokesKey = keyof ChannelInvokePayload;
