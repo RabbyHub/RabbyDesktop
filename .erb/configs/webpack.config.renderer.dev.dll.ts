3a378bcca12002a80aa485eee05a440863cb53f2
richardo2016x <richardo2016x@gmail.com>
2023-01-16 18:54:58 +0800

    chore: adjustment.

--- .erb/configs/webpack.config.renderer.dev.dll.ts
+++ .erb/configs/webpack.config.renderer.dev.dll.ts
@@ -33,7 +33,7 @@ const configuration: webpack.Configuration = {
   entry: {
     renderer: Object.keys(dependencies || {})
       .filter((dep) => !dep.startsWith('@types/'))
-      .filter((dep) => !['@debank/common', '@debank/rabby-api'].includes(dep)),
+      .filter((dep) => !['@debank/common', '@debank/rabby-api', '@rabby-wallet/rabby-swap'].includes(dep)),
   },
 
   output: {
--- src/renderer/routes-popup/QuickSwapWindow/index.tsx
+++ src/renderer/routes-popup/QuickSwapWindow/index.tsx
@@ -1,21 +1,39 @@
+import {
+  createHashRouter as createRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+
 import GlobalMask from '@/renderer/components/MainWindow/GlobalMask';
 import { hideMainwinPopupview } from '@/renderer/ipcRequest/mainwin-popupview';
 import styles from './index.module.less';
 import { SwapByDex } from './swap';
 
-export default function QuickSwapWindow() {
-  return (
-    <div className={styles.QuickSwapWindow}>
-      <GlobalMask
-        className={styles.mask}
-        onClick={() => {
-          hideMainwinPopupview('quick-swap');
-          // keep window state, don't reset
-        }}
-      />
-      <div className={styles.container}>
-        <SwapByDex />
+const router = createRouter([
+  {
+    path: '/',
+    id: 'quick-swap',
+    element: (
+      <div className={styles.QuickSwapWindow}>
+        <GlobalMask
+          className={styles.mask}
+          onClick={() => {
+            hideMainwinPopupview('quick-swap');
+            // keep window state, don't reset
+          }}
+        />
+        <div className={styles.container}>
+          <SwapByDex />
+        </div>
       </div>
-    </div>
-  );
+    ),
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" />,
+  },
+]);
+
+export default function QuickSwapWindow() {
+  return <RouterProvider router={router} />;
 }
