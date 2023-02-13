58c9befa57f45fb9d07ad249bf001d095df77883
richardo2016x <richardo2016x@gmail.com>
2023-02-13 19:21:55 +0800

    feat: upgrade rabbyx for new sign window style.

--- scripts/install_rabbyx.sh
+++ scripts/install_rabbyx.sh
@@ -5,7 +5,7 @@ project_dir=$(dirname "$script_dir")
 assets_dir="$( dirname "$scripts_dir"  )/assets"
 
 echo "[install_rabbyx] try to download rabbyx.zip..."
-curl -sL https://download.rabby.io/_tools/RabbyX-v0.68.0-93365932.zip > $assets_dir/rabbyx.zip
+curl -sL https://download.rabby.io/_tools/RabbyX-v0.68.0-69ec74a2.zip > $assets_dir/rabbyx.zip
 # curl -sL https://download.rabby.io/_tools/RabbyX-latest.zip > $assets_dir/rabbyx.zip
 
 mkdir -p $assets_dir/chrome_exts/
