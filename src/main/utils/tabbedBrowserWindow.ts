663d489914a2fe35251a452b4d452ce8bd1e41c1
richardo2016x <richardo2016x@gmail.com>
2023-02-24 17:58:13 +0800

    feat: adjust position of rabbyx's sign windows.

--- src/main/utils/tabbedBrowserWindow.ts
+++ src/main/utils/tabbedBrowserWindow.ts
@@ -7,14 +7,8 @@ import { getBrowserWindowOpts } from './app';
 import {
   getElectronChromeExtensions,
   getWebuiExtension,
-  onMainWindowReady,
-  RABBYX_WINDOWID_S,
-  toggleMaskViaOpenedRabbyxNotificationWindow,
 } from './stream-helpers';
-import {
-  getRabbyxNotificationBounds,
-  getWindowFromWebContents,
-} from './browser';
+import { getWindowFromWebContents } from './browser';
 
 import { isEnableContentProtected } from '../store/desktopApp';
 
@@ -96,65 +90,4 @@ export async function removeWindowRecord(win: Electron.BrowserWindow) {
   }
 
   return tabbedWin;
-}
-
-const isWin32 = process.platform === 'win32';
-
-export async function createRabbyxNotificationWindow({
-  url,
-  width,
-}: {
-  url: string;
-  width?: number;
-  height?: number;
-}) {
-  const mainWin = await onMainWindowReady();
-
-  const expectedBounds = getRabbyxNotificationBounds(mainWin.window);
-
-  const win = await createWindow({
-    defaultTabUrl: url,
-    windowType: 'popup',
-    isRabbyXNotificationWindow: true,
-    window: {
-      frame: false,
-      /**
-       * @notice by default, set transparent to true will
-       * lead all click behavior to be ignored (passthrough),
-       *
-       * but in this case, we provide a popup-view as gasket, which is
-       * under this window and above the main window, so we can set
-       * transparent to true and make borderless-style window.
-       */
-      transparent: true,
-      ...(!isWin32 && {
-        roundedCorners: true,
-        hasShadow: false,
-      }),
-      movable: false,
-      maximizable: false,
-      minimizable: false,
-      fullscreenable: false,
-      resizable: false,
-      parent: mainWin.window,
-      width: Math.min(width || expectedBounds.width, expectedBounds.width),
-      height: expectedBounds.height,
-      x: expectedBounds.x,
-      y: expectedBounds.y,
-      type: 'popup',
-    },
-  });
-
-  const windowId = win.window.id;
-  win.window.on('closed', () => {
-    RABBYX_WINDOWID_S.delete(windowId);
-    toggleMaskViaOpenedRabbyxNotificationWindow();
-  });
-
-  win.tabs.tabList[0]?._patchWindowClose();
-
-  RABBYX_WINDOWID_S.add(windowId);
-  toggleMaskViaOpenedRabbyxNotificationWindow();
-
-  return win.window as BrowserWindow;
 }
