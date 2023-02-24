afc3c74d0833779d0ea8c4481f02a95e7ef6e9df
richardo2016x <richardo2016x@gmail.com>
2023-02-24 18:07:55 +0800

    chore: upgrade rabbyx.

--- scripts/install_rabbyx.sh
+++ scripts/install_rabbyx.sh
@@ -5,7 +5,7 @@ project_dir=$(dirname "$script_dir")
 assets_dir="$( dirname "$scripts_dir"  )/assets"
 
 echo "[install_rabbyx] try to download rabbyx.zip..."
-curl -sL https://download.rabby.io/_tools/RabbyX-v0.68.0-12e0ae70.zip > $assets_dir/rabbyx.zip
+curl -sL https://download.rabby.io/_tools/RabbyX-v0.69.1-bec7ef79.zip > $assets_dir/rabbyx.zip
 # curl -sL https://download.rabby.io/_tools/RabbyX-latest.zip > $assets_dir/rabbyx.zip
 
 mkdir -p $assets_dir/chrome_exts/
