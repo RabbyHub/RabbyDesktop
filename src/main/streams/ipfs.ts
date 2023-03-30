c7ab5c2641fafb24ccd55a465df7ee64b7122229
richardo2016x <richardo2016x@gmail.com>
2023-03-30 22:55:06 +0800

    feat: adjust rootPath for IpfsService.

--- src/main/streams/ipfs.ts
+++ src/main/streams/ipfs.ts
@@ -1,15 +1,25 @@
-import { app } from 'electron';
-import { create } from 'ipfs-http-client';
 import path from 'path';
+import { create } from 'ipfs-http-client';
 import { IpfsService } from '../services/ipfs';
 import { handleIpcMainInvoke } from '../utils/ipcMainEvents';
+import { getAppUserDataPath } from '../utils/store';
+import { onMainWindowReady } from '../utils/stream-helpers';
 
-const ipfsService = new IpfsService({
-  ipfs: create(),
-  rootPath: path.join(app.getPath('userData'), 'ipfs-store'),
-});
+let gIpfsService: IpfsService;
+async function getIpfsService() {
+  await onMainWindowReady();
+  if (!gIpfsService) {
+    gIpfsService = new IpfsService({
+      ipfs: create(),
+      rootPath: path.join(getAppUserDataPath(), './local_cache/ipfs-store'),
+    });
+  }
+
+  return gIpfsService;
+}
 
 handleIpcMainInvoke('download-ipfs', async (_, ipfsString) => {
+  const ipfsService = await getIpfsService();
   const result = await ipfsService.download(ipfsString);
   if (result.errors?.length) {
     return {
