a468e3e0290d5faf0e216d8d1f8fd4b9b2c07a9e
richardo2016x <richardo2016x@gmail.com>
2023-04-13 22:42:44 +0800

    feat: support read system http proxy config on darwin.

--- src/main/utils/systemConfig.ts
+++ src/main/utils/systemConfig.ts
@@ -1,30 +1,69 @@
 import child_process from 'child_process';
 
-import { type ProxySetting, getProxyWindows } from 'get-proxy-settings';
+import {
+  type ProxySetting,
+  type Protocol,
+  getProxyWindows,
+} from 'get-proxy-settings';
 import type { AxiosRequestConfig } from 'axios';
 
 import { coerceNumber } from '@/isomorphic/primitive';
-import { formatProxyServerURL } from '@/isomorphic/url';
+import { parseScUtilProxyOutput } from '@/isomorphic/parser';
+
+const isWin32 = process.platform === 'win32';
 
 const command = 'scutil --proxy';
-async function getDarwinSystemProxySettings(): Promise<ProxySetting | null> {
-  const output = child_process.execSync(command);
-  // var settings = parseSettings(output);
-  // return settings;
+
+type ISimpleProxySetting = {
+  host: ProxySetting['host'];
+  port: ProxySetting['port'] | number;
+  protocol: ProxySetting['protocol'];
+  // TODO: use those two fields in proper case if provided
+  credentials?: ProxySetting['credentials'];
+  bypassAddrs?: string[];
+};
+
+async function getDarwinSystemHttpProxySettings(): Promise<ISimpleProxySetting | null> {
+  try {
+    const output = child_process.execSync(command);
+    const settings = parseScUtilProxyOutput(output.toString());
+
+    if (settings.HTTPEnable) {
+      return {
+        protocol: 'http' as Protocol,
+        host: settings.HTTPProxy as string,
+        port: settings.HTTPPort as number,
+      };
+    }
+    if (settings.HTTPSEnable) {
+      return {
+        protocol: 'http' as Protocol,
+        host: settings.HTTPSProxy as string,
+        port: settings.HTTPSPort as number,
+      };
+    } /* else if (settings.SOCKSEnable) {
+      return {
+        protocol: 'socks' as Protocol,
+        host: settings.SOCKSProxy as string,
+        port: settings.SOCKSPort as number,
+      }
+    } */
+  } catch (err) {
+    console.error(err);
+  }
 
   return null;
 }
 
-async function getWin32SystemProxySettings(): Promise<ProxySetting | null> {
+async function getWin32SystemHttpProxySettings(): Promise<ISimpleProxySetting | null> {
   const settings = await getProxyWindows();
 
   return settings?.https || settings?.http || null;
 }
 
-let systemProxyURL: string | false = false;
 type IGetSystemProxyServer = {
   proxyURL: string;
-  config: ProxySetting | null;
+  config: ISimpleProxySetting | null;
   configForAxios: Exclude<AxiosRequestConfig['proxy'], false> | null;
 };
 export async function getSystemProxyServer(forceReload = false) {
@@ -34,19 +73,11 @@ export async function getSystemProxyServer(forceReload = false) {
     configForAxios: null,
   };
   if ((result.proxyURL as any) === false || forceReload) {
-    result.config =
-      process.platform === 'win32'
-        ? await getWin32SystemProxySettings()
-        : await getDarwinSystemProxySettings();
+    result.config = isWin32
+      ? await getWin32SystemHttpProxySettings()
+      : await getDarwinSystemHttpProxySettings();
     if (result.config) {
       const port = coerceNumber(result.config.port, 0);
-
-      systemProxyURL = formatProxyServerURL({
-        // TODO: maybe we need to support https here
-        protocol: result.config.protocol as any,
-        hostname: result.config.host,
-        port,
-      });
 
       result.configForAxios = {
         protocol: result.config.protocol,
