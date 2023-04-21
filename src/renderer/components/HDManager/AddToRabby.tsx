c9ae18d7e0484f4fd62fa5a70eb80a582f5281a3
DMY <147dmy@gmail.com>
2023-04-22 00:30:10 +0800

    fix

--- src/renderer/components/HDManager/AddToRabby.tsx
+++ src/renderer/components/HDManager/AddToRabby.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import RcSwitch from 'rc-switch';
 import { LoadingOutlined } from '@ant-design/icons';
 import { Spin } from 'antd';
-import { useClickOutSide } from '@/renderer/hooks/useClick';
 import { TipsWrapper } from '../TipWrapper';
 
 const LoadingIcon = (
@@ -17,46 +16,21 @@ interface Props {
 export const AddToRabby: React.FC<Props> = ({ checked, onChange }) => {
   const [locked, setLocked] = React.useState(false);
 
-  const [showClickedTips, setShowClickedTips] = useState<true | undefined>();
   const buttonRef = useRef<HTMLButtonElement>(null);
   const timerRef = useRef<NodeJS.Timeout>();
 
-  useClickOutSide(buttonRef, () => {
-    setShowClickedTips(undefined);
-  });
-
-  const handleClickTips = useCallback(() => {
-    setShowClickedTips(true);
-    timerRef.current = setTimeout(() => {
-      setShowClickedTips(undefined);
-    }, 3000);
+  const handleOnChange = React.useCallback(async (value: boolean) => {
+    setLocked(true);
+    await onChange?.(value);
+    setLocked(false);
   }, []);
-
-  const handleOnChange = React.useCallback(
-    async (value: boolean) => {
-      setLocked(true);
-      await onChange?.(value);
-      setLocked(false);
-
-      handleClickTips();
-    },
-    [handleClickTips]
-  );
 
   useEffect(() => () => clearTimeout(timerRef.current), []);
 
   return (
     <TipsWrapper
-      open={showClickedTips}
-      hoverTips={
-        showClickedTips
-          ? checked
-            ? 'Added'
-            : 'Removed'
-          : checked
-          ? 'Remove from Rabby'
-          : 'Add to Rabby'
-      }
+      clickTips={checked || (!checked && locked) ? 'Added' : 'Removed'}
+      hoverTips={checked ? 'Remove from Rabby' : 'Add to Rabby'}
     >
       <RcSwitch
         ref={buttonRef}
