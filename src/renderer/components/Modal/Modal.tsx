14d609f0d411adf9a11248dff496b0656e3cd54c
DMY <147dmy@gmail.com>
2023-04-25 23:18:51 +0800

    fix (#385)
    * fix
    
    * fix:dapp url copy
    
    * chore: tuning.
    
    ---------
    
    Co-authored-by: richardo2016x <richardo2016x@gmail.com>

--- src/renderer/components/Modal/Modal.tsx
+++ src/renderer/components/Modal/Modal.tsx
@@ -23,6 +23,8 @@ export const Modal: React.FC<Props> = ({
   return (
     <AntdModal
       width={1000}
+      maskTransitionName=""
+      transitionName=""
       {...props}
       className={classNames('RabbyModal', props.className, {
         'small-title': smallTitle,
