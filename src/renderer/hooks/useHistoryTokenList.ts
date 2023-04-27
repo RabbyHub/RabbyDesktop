4c90bf9d37dc3be3797895510f13c44215dd973a
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-04-27 22:17:28 +0800

    fix: typo

--- src/renderer/hooks/useHistoryTokenList.ts
+++ src/renderer/hooks/useHistoryTokenList.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import BigNumber from 'bignumber.js';
 import PQueue from 'p-queue';
-import { groupBy, set } from 'lodash';
+import { groupBy } from 'lodash';
 import { TokenItem } from '@debank/rabby-api/dist/types';
 import { walletOpenapi } from '@/renderer/ipcRequest/rabbyx';
 import { VIEW_TYPE } from '@/renderer/routes/Home/type';
