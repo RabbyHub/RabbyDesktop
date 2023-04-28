4eedad8ad5180e48b80675fe5b462e7e6722b70c
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2023-04-28 15:09:25 +0800

    feat: upgrade rabbyx to 0.81.0 (#391)
    * style: tuning, make connect window aligh top-right of dappView.
    
    * style: same change about alignment with dappView, but on macOS.
    
    * feat: upgrade rabbyx.

--- src/main/utils/browserSize.ts
+++ src/main/utils/browserSize.ts
@@ -1,7 +1,6 @@
 import { NativeAppSizes } from '@/isomorphic/const-size-next';
 
+const isWin32 = process.platform === 'win32';
 export function getMainWindowTopOffset() {
-  return process.platform === 'darwin'
-    ? 0
-    : NativeAppSizes.windowTitlebarHeight;
+  return isWin32 ? NativeAppSizes.windowTitlebarHeight : 0;
 }
