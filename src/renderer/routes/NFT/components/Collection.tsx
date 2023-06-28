871ee1428269362d35b709bc47189535bbe8fa8f
DMY <147dmy@gmail.com>
2023-06-28 13:42:34 +0800

    chore: remove useless code

--- src/renderer/routes/NFT/components/Collection.tsx
+++ src/renderer/routes/NFT/components/Collection.tsx
@@ -10,52 +10,6 @@ type CollectionProps = {
   onStart: (item: CollectionList) => void;
 };
 
-// const item: CollectionList = {
-//   id: '0x5d666f215a85b87cb042d59662a7ecd2c8cc44e6',
-//   chain: 'matic',
-//   name: 'Galaxy OAT - TJiioVCGDQ',
-//   description: null,
-//   logo_url:
-//     'https://static.debank.com/image/nft_collection/logo_url/galaxy-oat-tjiiovcgdq/2d86d54bf0866443f7fc2a642a320265.png',
-//   is_verified: true,
-//   credit_score: 1934686.362548698,
-//   receive_addr_count: 329437,
-//   is_scam: false,
-//   is_suspicious: false,
-//   is_core: true,
-//   floor_price: 0,
-//   nft_list: [
-//     {
-//       id: '6789886d5ea87cdda3ebc26626576456',
-//       contract_id: '0x5d666f215a85b87cb042d59662a7ecd2c8cc44e6',
-//       inner_id: '11561146',
-//       chain: 'matic',
-//       name: 'Welcome to Pocket Universe (OAT)',
-//       description: '',
-//       content_type: 'image',
-//       content: '',
-//       thumbnail_url: '',
-//       total_supply: 1,
-//       detail_url:
-//         'https://opensea.io/assets/matic/0x5d666f215a85b87cb042d59662a7ecd2c8cc44e6/11561146',
-//       attributes: [
-//         {
-//           trait_type: 'category',
-//           value: 'Welcome to Pocket Universe (OAT)',
-//         },
-//         {
-//           display_type: 'date',
-//           trait_type: 'birthday',
-//           value: 1680833760,
-//         },
-//       ],
-//       collection_id: 'matic:0x5d666f215a85b87cb042d59662a7ecd2c8cc44e6',
-//       contract_name: 'Galaxy OAT',
-//       is_erc721: true,
-//       amount: 1,
-//     },
-//   ],
-// };
 export const Collection = (props: CollectionProps) => {
   const { start, onPreview, item, onStart } = props;
 
@@ -100,32 +54,6 @@ export const Collection = (props: CollectionProps) => {
                 onPreview?.(e);
               }}
             />
-            // <div
-            //   key={e.id}
-            //   className="w-60 h-60 rounder-4 overflow-hidden relative group"
-            // >
-            //   <img
-            //     className="w-full h-full"
-            //     src={e.thumbnail_url || e.detail_url}
-            //     onError={(event) => {
-            //       event.currentTarget.src =
-            //         'rabby-internal://assets/icons/nft/default-nft.png';
-            //     }}
-            //   />
-            //   {
-            //     // e.total_supply > 1 &&
-            //     <div className="absolute top-4 right-4 rounded-[2px] bg-[#000] bg-opacity-40 px-[4px] py-[1px] text-white text-opacity-80 text-12 h-16">
-            //       x{e.total_supply}
-            //     </div>
-            //   }
-            //   <div className="absolute top-0 left-0 w-full h-full bg-[#000] bg-opacity-5 hidden items-center justify-center cursor-pointer group-hover:flex">
-            //     <img
-            //       className="w-20 h-20"
-            //       src="rabby-internal://assets/icons/nft/view.svg"
-            //     />
-            //     d
-            //   </div>
-            // </div>
           );
         })}
       </div>
