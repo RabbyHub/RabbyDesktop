d644c06da38739882db659cf0e74a97d6f26d4ea
Hodor <lost1q84@gmail.com>
2023-11-06 16:58:46 +0800

    feat: filter scam

---
+++ src/renderer/components/TransactionsModal/components/HistoryList.tsx
@@ -0,0 +1,81 @@
+import { useCurrentAccount } from '@/renderer/hooks/rabbyx/useAccount';
+import clsx from 'clsx';
+import { range } from 'lodash';
+import { useEffect, useRef } from 'react';
+import { useTxHistory } from '../hooks/useTxHistory';
+import { useTxSource } from '../hooks/useTxSource';
+import styles from '../index.module.less';
+import { Empty } from './Empty';
+import { Loading } from './Loading';
+import { TransactionItem } from './TransactionItem';
+
+export const HistoryList = ({
+  testnet = false,
+  isFilterScam,
+}: {
+  testnet?: boolean;
+  isFilterScam?: boolean;
+}) => {
+  const { currentAccount } = useCurrentAccount();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { data, loading, loadingMore, mutate } = useTxHistory(
+    currentAccount?.address as unknown as string,
+    ref,
+    testnet,
+    isFilterScam
+  );
+
+  useEffect(() => {
+    mutate({
+      last: 0,
+      list: [],
+    });
+    ref?.current?.scrollTo(0, 0);
+  }, [currentAccount?.address, mutate]);
+
+  const isEmpty = !loading && data?.list.length === 0;
+
+  const source = useTxSource(currentAccount?.address as unknown as string);
+
+  return (
+    <>
+      {loading && isFilterScam ? (
+        <div className="text-r-neutral-body text-center text-[12px] leading-[14px] pt-[8px] pb-[22px]">
+          Loading may take a moment, and data delays are possible
+        </div>
+      ) : null}
+
+      <div
+        ref={ref}
+        className={clsx(styles.page, isFilterScam && !loading && 'mt-[30px]')}
+      >
+        {isEmpty ? (
+          <Empty />
+        ) : (
+          <div className={styles.container}>
+            {loading ? (
+              range(5).map((i) => <Loading key={i} />)
+            ) : (
+              <>
+                {data?.list.map((item) => {
+                  return (
+                    <TransactionItem
+                      key={item.id}
+                      data={item}
+                      projectDict={item.projectDict}
+                      cateDict={item.cateDict}
+                      tokenDict={item.tokenDict || item.tokenUUIDDict || {}}
+                      origin={source?.get([item.chain, item.id].join('|'))}
+                    />
+                  );
+                })}
+                {loadingMore && <Loading />}
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
