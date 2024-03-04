bd323563e7eaf057a5b1c49a37f0436fb2cec27c
richardo2016x <104543757+richardo2016x@users.noreply.github.com>
2024-03-04 23:07:33 +0800

    build: sign windows binary with smctl. (#618)
    * build: sign windows binary with smctl.
    
    * build: robust change.
    
    * chore: typo fix.

---
+++ .erb/scripts/winSign.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.fingerprint = process.env.RABBY_DESKTOP_CODE_SIGINING_CERT_FINGERPRINT;
+
+const fingerprint = exports.fingerprint;
+
+exports.default = async function (configuration) {
+  if (configuration.path) {
+    console.log(`[winSign] try to sign ${configuration.path}...`)
+    require('child_process').execSync(
+      `smctl sign --fingerprint=${fingerprint} --input "${String(
+        configuration.path
+      )}"`
+    );
+    console.log(`[winSign] finish sign ${configuration.path}...`)
+  } else {
+    console.log('[winSign] configuration.path is empty, skipped')
+  }
+};
