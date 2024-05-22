836f36790a3b4fcc17309971f32b2e264866631a
richardo2016x <richardo2016x@gmail.com>
2024-05-22 17:42:24 +0800

    feat: allow to close sign window on clicking most area in mask gasket, but the area covering header block.

--- src/renderer/routes-popup/RabbyNotificationGasket/index.tsx
+++ src/renderer/routes-popup/RabbyNotificationGasket/index.tsx
@@ -1,10 +1,14 @@
 import GlobalMask from '@/renderer/components/MainWindow/GlobalMask';
 
+import { detectClientOS } from '@/isomorphic/os';
 import styles from './index.module.scss';
+
+const isDarwin = detectClientOS() === 'darwin';
 
 export default function RabbyNotificationGasket() {
   return (
     <div className={styles.RabbyNotificationGasket}>
+      {isDarwin && <div className={styles.macOSDragger} />}
       <GlobalMask
         className={styles.maskInGasket}
         onClick={() => {
