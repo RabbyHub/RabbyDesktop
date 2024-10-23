fa2f790c168795c3426b5b06e24b856c816d42c2
hz002 <zs4733525@gmail.com>
2024-10-23 18:49:16 +0800

    fix: remove bluer effect

--- src/renderer/routes/ApprovalManagePage/components/NameAndAddr.tsx
+++ src/renderer/routes/ApprovalManagePage/components/NameAndAddr.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import NameAndAddress from '@/renderer/components/NameAndAddress';
 
-import clsx from 'clsx';
-import IconCopy from '../icons/icon-copy.svg';
-
 type Props = Omit<React.ComponentProps<typeof NameAndAddress>, ''>;
 
 export default function ApprovalsNameAndAddr({
   copyIconClass,
   ...props
 }: Props) {
-  return (
-    <NameAndAddress
-      {...props}
-      copyIcon={IconCopy}
-      copyIconOpacity={100}
-      copyIconClass={clsx(copyIconClass, 'w-[16px] h-[16px] inline-block')}
-    />
-  );
+  return <NameAndAddress {...props} copyIcon={false} />;
 }
