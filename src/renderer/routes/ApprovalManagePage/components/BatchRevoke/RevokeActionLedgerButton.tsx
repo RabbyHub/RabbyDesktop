831c314915eb9387a8f248a09985964bb993858b
hz002 <zs4733525@gmail.com>
2024-10-24 23:35:25 +0800

    fix: batch revoke ui error

--- src/renderer/routes/ApprovalManagePage/components/BatchRevoke/RevokeActionLedgerButton.tsx
+++ src/renderer/routes/ApprovalManagePage/components/BatchRevoke/RevokeActionLedgerButton.tsx
@@ -76,7 +76,7 @@ export const RevokeActionLedgerButton: React.FC<{
       <Modal
         className="confirm-revoke-modal ledger-modal"
         width={400}
-        visible={visibleLedgerConnectModal}
+        open={visibleLedgerConnectModal}
         onCancel={() => setVisibleLedgerConnectModal(false)}
         title={t('page.dashboard.hd.ledgerIsDisconnected')}
       >
