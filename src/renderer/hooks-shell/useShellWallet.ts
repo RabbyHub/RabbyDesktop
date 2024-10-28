c70f83272d97fffd7a9c3626993d4b1a5f7ccd52
richardo2016x <richardo2016x@gmail.com>
2024-10-28 15:36:48 +0800

    fix: make `sendTransaction` adapt to desktop.

--- src/renderer/hooks-shell/useShellWallet.ts
+++ src/renderer/hooks-shell/useShellWallet.ts
@@ -1,11 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { createRef, useEffect, useRef } from 'react';
 import { atom, useAtom } from 'jotai';
 
 import { IS_RUNTIME_PRODUCTION } from '@/isomorphic/constants';
 import { ShellWalletType, makeShellWallet } from '../utils-shell/shell-wallet';
 
 const shellWalletAtom = atom(null as any as ShellWalletType);
+const shellWalletRef = {
+  current: null as null | ShellWalletType,
+};
 
+export function getUIShellWallet() {
+  return shellWalletRef.current;
+}
 /**
  * @description make sure you component is child of <ShellWalletProvider />
  *
@@ -13,17 +19,16 @@ const shellWalletAtom = atom(null as any as ShellWalletType);
 export function useShellWallet() {
   const [shellWallet, setShellWallet] = useAtom(shellWalletAtom);
 
-  const instanceRef = useRef<ShellWalletType | null>(shellWallet);
   useEffect(() => {
-    if (instanceRef.current) return;
+    if (shellWalletRef.current) return;
 
     window.rabbyDesktop?.ipcRenderer
       .invoke('__internal_invoke:rabbyx:waitExtBgGhostLoaded')
       .then(({ rabbyxExtId }) => {
-        if (instanceRef.current) return;
+        if (shellWalletRef.current) return;
 
         const wallet = makeShellWallet(rabbyxExtId);
-        instanceRef.current = wallet;
+        shellWalletRef.current = wallet;
 
         if (!IS_RUNTIME_PRODUCTION) {
           (window as any).shellWallet = wallet;
@@ -34,4 +39,8 @@ export function useShellWallet() {
   }, [setShellWallet]);
 
   return shellWallet;
+}
+
+export function useTriggerShellWalletOnTop() {
+  useShellWallet();
 }
