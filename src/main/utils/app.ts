ea6f569170d6b0a60be47e939a6bcb779d5209e2
hz002 <zs4733525@gmail.com>
2024-10-29 15:23:50 +0800

    feat: revert dev code

--- src/main/utils/app.ts
+++ src/main/utils/app.ts
@@ -136,8 +136,8 @@ export function getBrowserWindowOpts(
       // enableRemoteModule: false,
       contextIsolation: true,
       // worldSafeExecuteJavaScript: true,
-      ...windowOpts?.webPreferences,
       devTools: IS_DEVTOOLS_AVAILBLE,
+      ...windowOpts?.webPreferences,
     },
   };
 }
