15c4bc5dee29e9091112ccc7cf8ba50a4f8b9c14
DMY <147dmy@gmail.com>
2024-12-12 11:43:50 +0800

    feat: swap support 0xv2 (#678)
    * feat: swap support 0xv2
    
    * fix: topup disabled token

--- src/renderer/routes/Swap/component/Transactions.tsx
+++ src/renderer/routes/Swap/component/Transactions.tsx
@@ -217,7 +217,9 @@ const Transaction = forwardRef<HTMLDivElement, TransactionProps>(
     const isCompleted = data?.status === 'Completed';
     const time = data?.finished_at || data?.create_at;
     const targetDex =
-      DEX?.[data?.dex_id as keyof typeof DEX]?.name || data?.dex_id || '';
+      data?.dex_id === '0xV2'
+        ? '0x'
+        : DEX?.[data?.dex_id]?.name || data?.dex_id || '';
     const txId = data?.tx_id;
     const chainItem = findChain({
       serverId: data?.chain,
--- src/renderer/routes/Swap/utils.ts
+++ src/renderer/routes/Swap/utils.ts
@@ -115,7 +115,7 @@ export const postSwap = async ({
       slippage: new BigNumber(slippage).div(100).toNumber(),
     },
     // 0xAPI => 0x
-    dex_id: dexId.replace('API', ''),
+    dex_id: dexId,
     tx_id: txId,
     tx,
   });
