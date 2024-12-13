8f1eced5e5b036ff61f43b753bd15410d631bf60
DMY <147dmy@gmail.com>
2024-12-13 17:17:36 +0800

    chore: clean up

--- src/renderer/components/MainWindow/index.tsx
+++ src/renderer/components/MainWindow/index.tsx
@@ -228,15 +228,6 @@ const router = createRouter([
             <Bridge />
           </>
         ),
-        // loader: () => {
-        //   return {
-        //     title: (
-        //       <div className="absolute w-[1080px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        //         Bridge
-        //       </div>
-        //     ),
-        //   } as MainWindowRouteData;
-        // },
       },
       {
         path: 'dapps/:dappId',
