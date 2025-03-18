27199a38e0cb19220ae6cba8be8aed7abf45bf55
Hodor <lost1q84@gmail.com>
2025-03-18 14:25:19 +0800

    fix: safe (#694)
    * fix: safe
    
    * feat: use parseTx replace explain

--- src/renderer/routes/Settings/components/SignatureRecordModal/TransactionHistory/components/TransactionExplain.tsx
+++ src/renderer/routes/Settings/components/SignatureRecordModal/TransactionHistory/components/TransactionExplain.tsx
@@ -1,10 +1,26 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { TransactionHistoryItem } from '@/isomorphic/types/rabbyx';
+import {
+  TransactionGroup,
+  TransactionHistoryItem,
+} from '@/isomorphic/types/rabbyx';
 import { isSameAddress } from '@/renderer/utils/address';
 import { splitNumberByStep } from '@/renderer/utils/number';
 import { ExplainTxResponse } from '@rabby-wallet/rabby-api/dist/types';
+import { Trans, useTranslation } from 'react-i18next';
+import {
+  ApproveNFTRequireData,
+  ApproveTokenRequireData,
+  ContractCallRequireData,
+  ContractRequireData,
+  RevokeNFTRequireData,
+  RevokeTokenApproveRequireData,
+  SwapRequireData,
+  WrapTokenRequireData,
+} from '@rabby-wallet/rabby-action';
+import { getTokenSymbol } from '@/renderer/utils';
+import { getActionTypeText } from '@/renderer/utils/action';
 
 const IconUnknown = 'rabby-internal://assets/icons/transaction/tx-unknown.svg';
 
@@ -15,149 +31,276 @@ export const TransactionExplain = ({
   isSubmitFailed,
   isWithdrawed,
   isCancel,
-  explain,
   onOpenScan,
+  action,
 }: {
   isFailed: boolean;
   isSubmitFailed: boolean;
   isWithdrawed: boolean;
   isCancel: boolean;
-  explain: ExplainTxResponse;
   onOpenScan(): void;
+  action: TransactionGroup['action'];
 }) => {
-  let icon: React.ReactNode = (
-    <img className="icon icon-explain" src={IconUnknown} />
-  );
-  let content: string | React.ReactNode = 'Unknown Transaction';
-  if (explain) {
-    if (explain.type_cancel_nft_collection_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_cancel_nft_collection_approval
-              .spender_protocol_logo_url || IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          Cancel NFT Collection Approval for{' '}
-          {explain.type_cancel_nft_collection_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_nft_collection_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_nft_collection_approval.spender_protocol_logo_url ||
-            IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          NFT Collection Approval for{' '}
-          {explain.type_nft_collection_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_cancel_single_nft_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_cancel_single_nft_approval.spender_protocol_logo_url ||
-            IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          Cancel Single NFT Approval for{' '}
-          {explain.type_cancel_single_nft_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_single_nft_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_single_nft_approval.spender_protocol_logo_url ||
-            IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          Single NFT Approval for{' '}
-          {explain.type_single_nft_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_nft_send) {
-      icon = <img className="icon icon-explain" src={IconUser} />;
-      content = `Send ${splitNumberByStep(
-        explain.type_nft_send.token_amount
-      )} NFT`;
-    } else if (explain.type_cancel_token_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_cancel_token_approval.spender_protocol_logo_url ||
-            IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          Cancel {explain.type_cancel_token_approval.token_symbol} Approve for{' '}
-          {explain.type_cancel_token_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_token_approval) {
-      icon = (
-        <img
-          src={
-            explain.type_token_approval.spender_protocol_logo_url || IconUnknown
-          }
-          className="icon icon-explain"
-        />
-      );
-      content = (
-        <>
-          Approve {explain.type_token_approval.token_symbol}{' '}
-          {explain.type_token_approval.is_infinity
-            ? 'unlimited'
-            : splitNumberByStep(explain.type_token_approval.token_amount)}{' '}
-          for{' '}
-          {explain.type_token_approval.spender_protocol_name ||
-            'Unknown protocol'}
-        </>
-      );
-    } else if (explain.type_send) {
-      icon = <img className="icon icon-explain" src={IconUser} />;
-      content = (
-        <>
-          Send {splitNumberByStep(explain.type_send.token_amount)}{' '}
-          {explain.type_send.token_symbol}
-        </>
-      );
-    } else if (explain.type_call) {
-      icon = (
-        <img
-          src={explain.type_call.contract_protocol_logo_url || IconUnknown}
-          className="icon icon-explain"
-        />
-      );
-      content = explain.type_call.action;
+  const { t } = useTranslation();
+
+  const { icon, content } = useMemo(() => {
+    let icon: React.ReactNode = (
+      <img className="icon icon-explain" src={IconUnknown} />
+    );
+    let content: string | React.ReactNode = t(
+      'page.activities.signedTx.explain.unknown'
+    );
+    if (action) {
+      const actionData = action.actionData;
+      content = getActionTypeText(actionData);
+      if (actionData.swap) {
+        const requiredData = action.requiredData as SwapRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.crossToken) {
+        const requiredData = action.requiredData as SwapRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.crossSwapToken) {
+        const requiredData = action.requiredData as SwapRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.wrapToken) {
+        const requiredData = action.requiredData as WrapTokenRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.unWrapToken) {
+        const requiredData = action.requiredData as WrapTokenRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.send) {
+        icon = <img className="icon icon-explain" src={IconUser} />;
+        content = `${t('page.activities.signedTx.explain.send', {
+          amount: splitNumberByStep(actionData.send.token.amount),
+          symbol: getTokenSymbol(actionData.send.token),
+        })}`;
+      } else if (actionData.approveToken) {
+        const requiredData =
+          action.requiredData as ApproveTokenRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.approve"
+            values={{
+              token: getTokenSymbol(actionData.approveToken.token),
+              count: actionData.approveToken.token.is_infinity
+                ? t('page.activities.signedTx.common.unlimited')
+                : splitNumberByStep(actionData.approveToken.token.amount || 0),
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData.revokeToken) {
+        const requiredData =
+          action.requiredData as RevokeTokenApproveRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.cancel"
+            values={{
+              token: actionData.revokeToken.token?.symbol,
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData.revokePermit2) {
+        const requiredData =
+          action.requiredData as RevokeTokenApproveRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.cancelTx) {
+        icon = <img src={IconUser} className="icon icon-explain" />;
+      } else if (actionData.sendNFT) {
+        icon = <img className="icon icon-explain" src={IconUser} />;
+        content = `${t('page.activities.signedTx.explain.send', {
+          amount: splitNumberByStep(actionData.sendNFT.nft.amount),
+          symbol: 'NFT',
+        })}`;
+      } else if (actionData.approveNFT) {
+        const requiredData =
+          action.requiredData as ApproveNFTRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.singleNFTApproval"
+            values={{
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData.revokeNFT) {
+        const requiredData = action.requiredData as RevokeNFTRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.cancelSingleNFTApproval"
+            values={{
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData?.revokeNFTCollection) {
+        const requiredData = action.requiredData as RevokeNFTRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.cancelNFTCollectionApproval"
+            values={{
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData.approveNFTCollection) {
+        const requiredData =
+          action.requiredData as ApproveNFTRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content = (
+          <Trans
+            i18nKey="page.activities.signedTx.explain.nftCollectionApproval"
+            values={{
+              protocol:
+                requiredData?.protocol?.name ||
+                t('page.activities.signedTx.common.unknownProtocol'),
+            }}
+            t={t}
+          />
+        );
+      } else if (actionData.deployContract) {
+        // todo
+      } else if (actionData.pushMultiSig) {
+        // todo
+      } else if (actionData.assetOrder) {
+        const requiredData = action.requiredData as ContractRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.transferOwner) {
+        // todo
+      } else if (actionData.multiSwap) {
+        const requiredData = action.requiredData as SwapRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.swapLimitPay) {
+        const requiredData = action.requiredData as SwapRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.contractCall) {
+        const requiredData =
+          action.requiredData as ContractCallRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+        content =
+          requiredData?.call?.func ||
+          t('page.activities.signedTx.explain.unknown');
+      } else if (actionData.common) {
+        const requiredData =
+          action.requiredData as ContractCallRequireData | null;
+        icon = (
+          <img
+            src={requiredData?.protocol?.logo_url || IconUnknown}
+            className="icon icon-explain"
+          />
+        );
+      } else if (actionData.permit2BatchRevokeToken) {
+        // todo
+      } else {
+        content = t('page.activities.signedTx.explain.unknown');
+      }
     }
-  }
+    return {
+      content,
+      icon,
+    };
+  }, [action, t]);
 
   return (
     <div className="tx-explain" onClick={onOpenScan}>
---
+++ src/renderer/utils/action.ts
@@ -0,0 +1,112 @@
+import i18n from 'i18next';
+import { ParsedTransactionActionData } from '@rabby-wallet/rabby-action';
+
+export const getActionTypeText = (data: ParsedTransactionActionData) => {
+  const t = i18n.t;
+
+  if (data.swap) {
+    return t('page.signTx.swap.title');
+  }
+  if (data.crossToken) {
+    return t('page.signTx.crossChain.title');
+  }
+  if (data.crossSwapToken) {
+    return t('page.signTx.swapAndCross.title');
+  }
+  if (data.wrapToken) {
+    return t('page.signTx.wrapToken');
+  }
+  if (data.unWrapToken) {
+    return t('page.signTx.unwrap');
+  }
+  if (data.send) {
+    return t('page.signTx.send.title');
+  }
+  if (data.approveToken) {
+    return t('page.signTx.tokenApprove.title');
+  }
+  if (data.revokeToken) {
+    return t('page.signTx.revokeTokenApprove.title');
+  }
+  if (data.sendNFT) {
+    return t('page.signTx.sendNFT.title');
+  }
+  if (data.approveNFT) {
+    return t('page.signTx.nftApprove.title');
+  }
+  if (data.revokeNFT) {
+    return t('page.signTx.revokeNFTApprove.title');
+  }
+  if (data.approveNFTCollection) {
+    return t('page.signTx.nftCollectionApprove.title');
+  }
+  if (data.revokeNFTCollection) {
+    return t('page.signTx.revokeNFTCollectionApprove.title');
+  }
+  if (data.deployContract) {
+    return t('page.signTx.deployContract.title');
+  }
+  if (data.cancelTx) {
+    return t('page.signTx.cancelTx.title');
+  }
+  if (data.pushMultiSig) {
+    return t('page.signTx.submitMultisig.title');
+  }
+  if (data.contractCall) {
+    return t('page.signTx.unknownAction');
+  }
+  if (data.revokePermit2) {
+    return t('page.signTx.revokePermit2.title');
+  }
+  if (data.assetOrder) {
+    return t('page.signTx.assetOrder.title');
+  }
+  if (data?.common) {
+    return data.common.title;
+  }
+  if (data.permit2BatchRevokeToken) {
+    return t('page.signTx.batchRevokePermit2.title');
+  }
+  if (data.transferOwner) {
+    return t('page.signTx.transferOwner.title');
+  }
+  if (data.swapLimitPay) {
+    return t('page.signTx.swapLimitPay.title');
+  }
+  if (data.multiSwap) {
+    return t('page.signTx.swap.title');
+  }
+  return t('page.signTx.unknownAction');
+};
+
+export const getActionTypeTextByType = (type: string) => {
+  const t = i18n.t;
+
+  const dict = {
+    swap_token: t('page.signTx.swap.title'),
+    cross_token: t('page.signTx.crossChain.title'),
+    cross_swap_token: t('page.signTx.swapAndCross.title'),
+    send_token: t('page.signTx.send.title'),
+    approve_token: t('page.signTx.tokenApprove.title'),
+    revoke_token: t('page.signTx.revokeTokenApprove.title'),
+    permit2_revoke_token: t('page.signTx.revokePermit2.title'),
+    wrap_token: t('page.signTx.wrapToken'),
+    unwrap_token: t('page.signTx.unwrap'),
+    send_nft: t('page.signTx.sendNFT.title'),
+    approve_nft: t('page.signTx.nftApprove.title'),
+    revoke_nft: t('page.signTx.revokeNFTApprove.title'),
+    approve_collection: t('page.signTx.nftCollectionApprove.title'),
+    revoke_collection: t('page.signTx.revokeNFTCollectionApprove.title'),
+    deploy_contract: t('page.signTx.deployContract.title'),
+    cancel_tx: t('page.signTx.cancelTx.title'),
+    push_multisig: t('page.signTx.submitMultisig.title'),
+    contract_call: t('page.signTx.contractCall.title'),
+    swap_order: t('page.signTx.assetOrder.title'),
+    permit2_batch_revoke_token: t('page.signTx.batchRevokePermit2.title'),
+    transfer_owner: t('page.signTx.transferOwner.title'),
+    multiSwap: t('page.signTx.swap.title'),
+    swapLimitPay: t('page.signTx.swapLimitPay.title'),
+  } as Record<string, string>;
+
+  return dict[type] || t('page.signTx.unknownAction');
+};
